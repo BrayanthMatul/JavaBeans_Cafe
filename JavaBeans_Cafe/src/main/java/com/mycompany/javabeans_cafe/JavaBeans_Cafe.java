d3f76463c836321a92b0b970ecaf89a2ea7c5814
matul <brayanth.matul146@gmail.com>
2026-08-03 00:30:44 -0600

    Estructura del Proyecto Inicial

---
+++ JavaBeans_Cafe/src/main/java/com/mycompany/javabeans_cafe/JavaBeans_Cafe.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javabeans_cafe;
+
+/**
+ *
+ * @author matul
+ */
+public class JavaBeans_Cafe {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

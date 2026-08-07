2e8190c45c3d1fae6969c55525f38cb33969d140
BrayanthMatul <brayanth.matul146@gmail.com>
2026-08-06 19:41:36 -0600

    Clase para verificar los datos al registrar un empleado

---
+++ JavaBeans_Cafe/src/main/java/com/mycompany/javabeans_cafe/util/VerificadorDatosEmpleado.java
@@ -0,0 +1,17 @@
+package com.mycompany.javabeans_cafe.util;
+
+import java.sql.SQLException;
+
+public class VerificadorDatosEmpleado {
+
+    public void verificarDatos(String nombreUsuario) throws SQLException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'verificarDatos'");
+    }
+
+    public boolean getExisteNombreUsuario() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getExisteNombreUsuario'");
+    }
+    
+}

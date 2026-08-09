24c48b60ca526444643fefaf160429ba9bb4b828
matul <brayanth.matul146@gmail.com>
2026-08-09 13:15:23 -0600

    Se agregaron metodos para insertar, obtenerPorNombre y obtenerTodos

--- JavaBeans_Cafe/src/main/java/com/mycompany/javabeans_cafe/daos/InsumoDAO.java
+++ JavaBeans_Cafe/src/main/java/com/mycompany/javabeans_cafe/daos/InsumoDAO.java
@@ -1,5 +1,79 @@
 package com.mycompany.javabeans_cafe.daos;
 
+import com.mycompany.javabeans_cafe.db.ConexionBD;
+import com.mycompany.javabeans_cafe.modelos.Insumo;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class InsumoDAO {
 
+    public void insertarInsumo(Insumo insumo) throws SQLException {
+        String query = "INSERT INTO insumo (nombre_insumo, unidad_medida, stock_actual, stock_minimo, costo_insumo) VALUES (?, ?, ?, ?, ?)";
+        
+        try (Connection conexion = ConexionBD.getConexion();
+                PreparedStatement preparedStatement = conexion.prepareStatement(query)) {
+            preparedStatement.setString(1, insumo.getNombreInsumo());
+            preparedStatement.setString(2, insumo.getUnidadMedida());
+            preparedStatement.setInt(3, insumo.getStockActual());
+            preparedStatement.setInt(4, insumo.getStockMinimo());
+            preparedStatement.setBigDecimal(5, insumo.getCostoInsumo());
+
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new SQLException("Error al insertar insumo en la base de datos: " + e.getMessage());
+        }
+    }
+
+    public Insumo obtenerPorNombre(String nombreInsumo) throws SQLException {
+        String query = "SELECT * FROM insumo WHERE nombre_insumo = ?";
+        
+        try (Connection conexion = ConexionBD.getConexion();
+                PreparedStatement preparedStatement = conexion.prepareStatement(query)) {
+            preparedStatement.setString(1, nombreInsumo);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                return convertirAInsumo(resultSet);
+            }
+        } catch (SQLException e) {
+            throw new SQLException("Error al obtener insumo por nombre: " + e.getMessage());
+        }
+        return null;
+    }
+
+    public List<Insumo> obtenerTodos() throws SQLException {
+        String query = "SELECT * FROM insumo";
+        List<Insumo> insumos = new ArrayList<>();
+
+        try (Connection conexion = ConexionBD.getConexion();
+                PreparedStatement preparedStatement = conexion.prepareStatement(query);
+                ResultSet resultSet = preparedStatement.executeQuery()) {
+
+            while (resultSet.next()) {
+                insumos.add(convertirAInsumo(resultSet));
+            }
+
+        } catch (SQLException e) {
+            throw new SQLException(
+                    "Error al obtener insumos: "
+                    + e.getMessage());
+        }
+        return insumos;
+    }
+
+    private Insumo convertirAInsumo(ResultSet resultSet) throws SQLException {
+        return new Insumo(
+                resultSet.getInt("codigo_insumo"),
+                resultSet.getString("nombre_insumo"),
+                resultSet.getString("unidad_medida"),
+                resultSet.getInt("stock_actual"),
+                resultSet.getInt("stock_minimo"),
+                resultSet.getBigDecimal("costo_insumo")
+        );
+    }
 }

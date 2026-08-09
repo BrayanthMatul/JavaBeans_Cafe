af1edcb57cd39da2fbe2541c4795e529d9615020
matul <brayanth.matul146@gmail.com>
2026-08-08 22:27:16 -0600

    Se implemento la verificacion desde la base de datos

--- JavaBeans_Cafe/src/main/java/com/mycompany/javabeans_cafe/util/VerificadorDatosEmpleado.java
+++ JavaBeans_Cafe/src/main/java/com/mycompany/javabeans_cafe/util/VerificadorDatosEmpleado.java
@@ -2,16 +2,24 @@ package com.mycompany.javabeans_cafe.util;
 
 import java.sql.SQLException;
 
+import com.mycompany.javabeans_cafe.daos.EmpleadoDAO;
+
 public class VerificadorDatosEmpleado {
 
-    public void verificarDatos(String nombreUsuario) throws SQLException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'verificarDatos'");
+    private final EmpleadoDAO empleadoDAO;
+
+    public VerificadorDatosEmpleado() {
+        empleadoDAO = new EmpleadoDAO();
     }
 
-    public boolean getExisteNombreUsuario() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getExisteNombreUsuario'");
+    public boolean existeNombreUsuario(String nombreUsuario)
+            throws SQLException {
+
+        return empleadoDAO.encontrarPorNombreUsuario(nombreUsuario) != null;
     }
-    
+
+    public boolean existeDPI(String dpi) throws SQLException {
+        return empleadoDAO.encontrarPorDPI(dpi) != null;
+    }
+
 }

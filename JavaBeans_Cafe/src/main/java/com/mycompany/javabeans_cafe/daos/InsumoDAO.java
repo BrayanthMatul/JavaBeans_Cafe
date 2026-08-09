2b5afc6938906b88e95c57d9cd38b19c25dfd1d2
matul <brayanth.matul146@gmail.com>
2026-08-09 15:45:52 -0600

    Se agrego el metodo para actualizar un insumo

--- JavaBeans_Cafe/src/main/java/com/mycompany/javabeans_cafe/daos/InsumoDAO.java
+++ JavaBeans_Cafe/src/main/java/com/mycompany/javabeans_cafe/daos/InsumoDAO.java
@@ -13,7 +13,7 @@ public class InsumoDAO {
 
     public void insertarInsumo(Insumo insumo) throws SQLException {
         String query = "INSERT INTO insumo (nombre_insumo, unidad_medida, stock_actual, stock_minimo, costo_insumo) VALUES (?, ?, ?, ?, ?)";
-        
+
         try (Connection conexion = ConexionBD.getConexion();
                 PreparedStatement preparedStatement = conexion.prepareStatement(query)) {
             preparedStatement.setString(1, insumo.getNombreInsumo());
@@ -31,7 +31,7 @@ public class InsumoDAO {
 
     public Insumo obtenerPorNombre(String nombreInsumo) throws SQLException {
         String query = "SELECT * FROM insumo WHERE nombre_insumo = ?";
-        
+
         try (Connection conexion = ConexionBD.getConexion();
                 PreparedStatement preparedStatement = conexion.prepareStatement(query)) {
             preparedStatement.setString(1, nombreInsumo);
@@ -61,9 +61,26 @@ public class InsumoDAO {
         } catch (SQLException e) {
             throw new SQLException(
                     "Error al obtener insumos: "
-                    + e.getMessage());
+                            + e.getMessage());
         }
         return insumos;
+    }
+
+    public void actualizarInsumo(Insumo insumo) throws SQLException {
+        String query = "UPDATE insumo SET nombre_insumo = ?, unidad_medida = ?, stock_actual = ?, stock_minimo = ?, costo_insumo = ? WHERE codigo_insumo = ?";
+
+        try (Connection conexion = ConexionBD.getConexion();
+                PreparedStatement preparedStatement = conexion.prepareStatement(query)) {
+            preparedStatement.setString(1, insumo.getNombreInsumo());
+            preparedStatement.setString(2, insumo.getUnidadMedida());
+            preparedStatement.setInt(3, insumo.getStockActual());
+            preparedStatement.setInt(4, insumo.getStockMinimo());
+            preparedStatement.setBigDecimal(5, insumo.getCostoInsumo());
+            preparedStatement.setInt(6, insumo.getCodigoInsumo());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new SQLException("Error al actualizar el insumo en la base de datos: " + e.getMessage());
+        }
     }
 
     private Insumo convertirAInsumo(ResultSet resultSet) throws SQLException {
@@ -73,7 +90,6 @@ public class InsumoDAO {
                 resultSet.getString("unidad_medida"),
                 resultSet.getInt("stock_actual"),
                 resultSet.getInt("stock_minimo"),
-                resultSet.getBigDecimal("costo_insumo")
-        );
+                resultSet.getBigDecimal("costo_insumo"));
     }
 }

3805ffb96bb0c420f13ba46314106a8b7e4d46a3
matul <brayanth.matul146@gmail.com>
2026-08-10 07:58:42 -0600

    Se agregaron getter y setter y se modifico un contructor para que solo dependa de la capacidad

--- JavaBeans_Cafe/src/main/java/com/mycompany/javabeans_cafe/modelos/Mesa.java
+++ JavaBeans_Cafe/src/main/java/com/mycompany/javabeans_cafe/modelos/Mesa.java
@@ -13,9 +13,33 @@ public class Mesa {
         this.estado = estado;
     }
 
-    public Mesa(int numeroMesa, int capacidad) {
-        this.numeroMesa = numeroMesa;
+    public Mesa(int capacidad) {
         this.capacidad = capacidad;
         this.estado = EstadoMesa.LIBRE;
     }
+
+    public int getNumeroMesa() {
+        return numeroMesa;
+    }
+
+    public void setNumeroMesa(int numeroMesa) {
+        this.numeroMesa = numeroMesa;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public EstadoMesa getEstado() {
+        return estado;
+    }
+
+    public void setEstado(EstadoMesa estado) {
+        this.estado = estado;
+    }
+
 }

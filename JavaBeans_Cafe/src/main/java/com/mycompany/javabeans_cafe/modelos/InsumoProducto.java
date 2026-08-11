f4e99a5b031ff210f3e12758f1d682451573c9a9
matul <brayanth.matul146@gmail.com>
2026-08-11 11:33:18 -0600

    Se agrego setter y gettes y se modifico el atributo cantidad

--- JavaBeans_Cafe/src/main/java/com/mycompany/javabeans_cafe/modelos/InsumoProducto.java
+++ JavaBeans_Cafe/src/main/java/com/mycompany/javabeans_cafe/modelos/InsumoProducto.java
@@ -1,21 +1,56 @@
 package com.mycompany.javabeans_cafe.modelos;
 
+import java.math.BigDecimal;
+
 public class InsumoProducto {
     private int id;
     private int codigoInsumo;
     private int codigoProducto;
-    private int cantidad;
+    private BigDecimal cantidad;
 
-    public InsumoProducto(int id, int codigoInsumo, int codigoProducto, int cantidad) {
+    public InsumoProducto(int id, int codigoInsumo, int codigoProducto, BigDecimal cantidad) {
         this.id = id;
         this.codigoInsumo = codigoInsumo;
         this.codigoProducto = codigoProducto;
         this.cantidad = cantidad;
     }
 
-    public InsumoProducto(int codigoInsumo, int codigoProducto, int cantidad) {
+    public InsumoProducto(int codigoInsumo, int codigoProducto, BigDecimal cantidad) {
         this.codigoInsumo = codigoInsumo;
         this.codigoProducto = codigoProducto;
         this.cantidad = cantidad;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCodigoInsumo() {
+        return codigoInsumo;
+    }
+
+    public void setCodigoInsumo(int codigoInsumo) {
+        this.codigoInsumo = codigoInsumo;
+    }
+
+    public int getCodigoProducto() {
+        return codigoProducto;
+    }
+
+    public void setCodigoProducto(int codigoProducto) {
+        this.codigoProducto = codigoProducto;
+    }
+
+    public BigDecimal getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(BigDecimal cantidad) {
+        this.cantidad = cantidad;
+    }
+
 }

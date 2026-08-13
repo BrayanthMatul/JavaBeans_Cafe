f9cddd3596104abe50338eff97b25c148b54d033
matul <brayanth.matul146@gmail.com>
2026-08-13 12:02:43 -0600

    Se agrego un metodo usando PagoPendienteAutomatico para que se generan pagos pendientes

--- JavaBeans_Cafe/src/main/java/com/mycompany/javabeans_cafe/JavaBeans_Cafe.java
+++ JavaBeans_Cafe/src/main/java/com/mycompany/javabeans_cafe/JavaBeans_Cafe.java
@@ -5,6 +5,8 @@
 package com.mycompany.javabeans_cafe;
 
 import com.mycompany.javabeans_cafe.interfaces_graficas.LoginFrame;
+import com.mycompany.javabeans_cafe.servicios.PagoPendienteAutomaticoServicio;
+import java.sql.SQLException;
 
 /**
  *
@@ -13,7 +15,19 @@ import com.mycompany.javabeans_cafe.interfaces_graficas.LoginFrame;
 public class JavaBeans_Cafe {
 
     public static void main(String[] args) {
+        ejecutarPagosPendientes();
         LoginFrame loginFrame = new LoginFrame();
         loginFrame.setVisible(true);
+    }
+    
+    private static void ejecutarPagosPendientes(){
+        try {
+        PagoPendienteAutomaticoServicio pagoAutomatico = new PagoPendienteAutomaticoServicio();
+        pagoAutomatico.ejecutarPagoAutomatico();
+            
+        } catch (SQLException e) {
+            System.out.println("Error al generar pagos automáticos: " + e.getMessage()
+            );
+        }
     }
 }

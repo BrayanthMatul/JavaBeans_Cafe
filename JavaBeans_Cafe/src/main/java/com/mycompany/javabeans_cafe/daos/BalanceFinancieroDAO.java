8370ad4f0d4a6d6ad19c8074adaea824d3d61a9b
matul <brayanth.matul146@gmail.com>
2026-08-13 13:41:26 -0600

    Se agrego la opcion para insertar balances

--- JavaBeans_Cafe/src/main/java/com/mycompany/javabeans_cafe/daos/BalanceFinancieroDAO.java
+++ JavaBeans_Cafe/src/main/java/com/mycompany/javabeans_cafe/daos/BalanceFinancieroDAO.java
@@ -1,5 +1,27 @@
 package com.mycompany.javabeans_cafe.daos;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import com.mycompany.javabeans_cafe.modelos.BalanceFinanciero;
+
 public class BalanceFinancieroDAO {
 
+    public void insertar(Connection conexion, BalanceFinanciero balanceFinanciero)
+            throws SQLException {
+        String query = "INSERT INTO balance_financiero (fecha_hora, monto_ingresos, monto_egresos, balance) VALUES (?, ?, ?, ?)";
+
+        try (PreparedStatement preparedStatement = conexion.prepareStatement(query)) {
+            preparedStatement.setTimestamp(1, balanceFinanciero.getFechaHora());
+            preparedStatement.setBigDecimal(2, balanceFinanciero.getMontoIngresos());
+            preparedStatement.setBigDecimal(3, balanceFinanciero.getMontoEgresos());
+            preparedStatement.setBigDecimal(4, balanceFinanciero.getBalance());
+
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new SQLException("Error al insertar el pedido en la base de datos: " + e.getMessage());
+        }
+    }
 }

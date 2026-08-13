754d0045821d8ad9cdfa8afaca619cd7b38e24ae
matul <brayanth.matul146@gmail.com>
2026-08-13 14:32:55 -0600

    se agrego metodo para traer todos y traer al ultimo

--- JavaBeans_Cafe/src/main/java/com/mycompany/javabeans_cafe/daos/BalanceFinancieroDAO.java
+++ JavaBeans_Cafe/src/main/java/com/mycompany/javabeans_cafe/daos/BalanceFinancieroDAO.java
@@ -2,8 +2,12 @@ package com.mycompany.javabeans_cafe.daos;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.mycompany.javabeans_cafe.db.ConexionBD;
 import com.mycompany.javabeans_cafe.modelos.BalanceFinanciero;
 
 public class BalanceFinancieroDAO {
@@ -24,4 +28,52 @@ public class BalanceFinancieroDAO {
             throw new SQLException("Error al insertar el pedido en la base de datos: " + e.getMessage());
         }
     }
+
+    public List<BalanceFinanciero> obtenerTodos() throws SQLException {
+        String query = "SELECT * FROM balance_financiero";
+
+        List<BalanceFinanciero> balances = new ArrayList<>();
+
+        try (Connection conexion = ConexionBD.getConexion();
+                PreparedStatement preparedStatement = conexion.prepareStatement(query);
+                ResultSet resultSet = preparedStatement.executeQuery()) {
+
+            while (resultSet.next()) {
+                balances.add(convertirABalanceFinanciero(resultSet));
+            }
+        } catch (SQLException e) {
+            throw new SQLException(
+                    "Error al obtener los balances financieros: "
+                            + e.getMessage());
+        }
+        return balances;
+    }
+
+    public BalanceFinanciero obtenerUltimo() throws SQLException {
+        String query = "SELECT * FROM balance_financiero ORDER BY id DESC LIMIT 1";
+
+        try (Connection conexion = ConexionBD.getConexion();
+                PreparedStatement preparedStatement = conexion.prepareStatement(query);
+                ResultSet resultSet = preparedStatement.executeQuery()) {
+
+            if (resultSet.next()) {
+                return convertirABalanceFinanciero(resultSet);
+            }
+
+        } catch (SQLException e) {
+            throw new SQLException("Error al obtener el último balance financiero: " + e.getMessage());
+        }
+
+        return null;
+    }
+
+    private BalanceFinanciero convertirABalanceFinanciero(ResultSet resultSet) throws SQLException {
+        return new BalanceFinanciero(
+                resultSet.getInt("id"),
+                resultSet.getTimestamp("fecha_hora"),
+                resultSet.getBigDecimal("monto_ingresos"),
+                resultSet.getBigDecimal("monto_egresos"),
+                resultSet.getBigDecimal("balance"));
+    }
+
 }

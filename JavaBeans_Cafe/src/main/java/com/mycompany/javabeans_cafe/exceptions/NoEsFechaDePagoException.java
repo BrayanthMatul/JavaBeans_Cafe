6bb341380d1465a3c890fcfc5d3040ac7b54791a
matul <brayanth.matul146@gmail.com>
2026-08-13 13:39:52 -0600

    Exception en caso de no ser fecha de pago

---
+++ JavaBeans_Cafe/src/main/java/com/mycompany/javabeans_cafe/exceptions/NoEsFechaDePagoException.java
@@ -0,0 +1,8 @@
+package com.mycompany.javabeans_cafe.exceptions;
+
+public class NoEsFechaDePagoException extends Exception {
+    public NoEsFechaDePagoException(String message) {
+        super(message);
+    }
+
+}

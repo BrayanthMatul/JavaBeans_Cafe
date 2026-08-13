be6f45a75b9feed0ad526fd924533db83297b506
matul <brayanth.matul146@gmail.com>
2026-08-13 09:23:39 -0600

    Exception en caso de tener stock bajo

---
+++ JavaBeans_Cafe/src/main/java/com/mycompany/javabeans_cafe/exceptions/StockInsuficienteException.java
@@ -0,0 +1,9 @@
+package com.mycompany.javabeans_cafe.exceptions;
+
+public class StockInsuficienteException extends Exception {
+
+    public StockInsuficienteException(String string) {
+        super(string);
+    }
+
+}
